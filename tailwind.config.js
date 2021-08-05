f805e74969e7550f235a3b6befd044cda166ea9c
grippal <lucagrip@yahoo.com>
2021-08-05 16:50:53 -0400

    Added styling, not complete yet

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,31 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            colors: {
+                blueGray: colors.blueGray,
+                coolGray: colors.coolGray,
+                trueGray: colors.trueGray,
+                warmGray: colors.warmGray,
+                orange: colors.orange,
+                amber: colors.amber,
+                lime: colors.lime,
+                emerald: colors.emerald,
+                teal: colors.teal,
+                cyan: colors.cyan,
+                sky: colors.sky,
+                indigo: colors.indigo,
+                violet: colors.violet,
+                fuchsia: colors.fuchsia,
+                rose: colors.rose
+            },
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
 }
